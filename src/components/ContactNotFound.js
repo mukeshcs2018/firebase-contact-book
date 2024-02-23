498f04bdc61d69aa72d7e0c3c493e8035ac09ee4
Mukesh <mukesh.cs2018@gmail.com>
2024-02-23 10:06:36 +0530

    added images

--- src/components/ContactNotFound.js
+++ src/components/ContactNotFound.js
@@ -1,10 +1,11 @@
 import React from "react";
+import contact from "../assets/hands_contact.png";
 
 const ContactNotFound = () => {
   return (
     <div className="flex h-[80vh] items-center justify-center gap-2">
       <div>
-        <img src="./images/Hands Contact.png" alt="contact-logo" />
+        <img src={contact} alt="contact-logo" />
       </div>
       <h3 className="text-white text-2xl font-semibold">Contact not found</h3>
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,9 @@
 import React from "react";
-
+import logo from "../assets/logos_firebase.svg";
 const NavBar = () => {
   return (
     <div className="bg-white h-[60px] my-4 rounded-lg flex items-center justify-center gap-2 text-lg ">
-      <img src="images/logos_firebase.svg" alt="logo" />
+      <img src={logo} alt="logo" />
       <h1>Firebase Contact App</h1>
     </div>
   );
